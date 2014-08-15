dc8f6bc9084ebf6a9850276e0e592ace2527396b
Denis matveev <disson@yandex.ru>
2014-08-15 12:21:59 +0600

    in sream version deleted unnecessary comments

--- cronbackup-stream.sh
+++ cronbackup-stream.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-set -x
+#set -x
 
 ######################
 ##### Settings ######################
@@ -49,15 +49,11 @@ makefull()
     TARGET="$BACKUPDIR"/"$NOW"/"$NOW"-0
     mkdir -p "$TARGET"
 ### backup ibdata ###
-    #/bin/nc -l -p 9999 | cat - > "$TARGET"/ibdatafullbackup.tar & 
-   # /bin/nc -l -p 9999 | tar ixvf > "$TARGET"/ & 
-    #ssh root@"$SERVER" "xtrabackup -v --backup  --datadir="$MY" --stream=tar | nc backup.navixy.com 9999 " #make full backups
-    #kill `pgrep "nc -l -p 9999" -f`
 
      cd "$TARGET" && ssh "$SERVER" xtrabackup --backup  --datadir="$MY" --stream=xbstream | xbstream -x
      ssh "$SERVER" tar zcf - "$MY"/xtrabackup_logfile | tar zxf - --strip-components=3 
 ### backup the rest files ###
-    #ssh matveev@flexo 'tar zcf - ~' > arch.tar.gz
+
     ssh "$SERVER" tar --create --preserve-permissions --totals --gzip  --ignore-failed-read --one-file-system --sparse\
     --wildcards --verbose \
     --listed-incremental=$MY/backup.list \
@@ -66,30 +62,23 @@ makefull()
     --exclude=xtrabackup_* \
     --exclude-caches "$MY" > "$TARGET"/mysql-0.tar.gz
 
-#   rsync -rv --exclude=ibdata* --exclude=xtrabackup_* root@"$SERVER":"$MY" "$TARGET"
-    
     return 0
 }
 
 makeincremental()
 {
+## backup ibdata    
     i=$(ls -1t "$BACKUPDIR"/"$CURRENTFULLBACKUP"/ 2>/dev/null | head -n1 | cut -d"-" -f3) # number of existing incremental backups
     PREVIOUSBACKUP=$(ls -1t "$BACKUPDIR"/"$CURRENTFULLBACKUP" | head -n1)
-    #let i=$i+1
     let "i+=1"
     TARGET="$BACKUPDIR"/"$CURRENTFULLBACKUP"/"$NOW"-"$i"
     mkdir -p "$TARGET"
 
     LAST_LSN=$(grep last_lsn "$BACKUPDIR"/"$CURRENTFULLBACKUP"/"$PREVIOUSBACKUP"/xtrabackup_checkpoints | cut -f3 -d" ")
-#    nc -l -p 9999 | cat - > "$TARGET"/ibdataincr$i.tar &
-    #ssh root@"$SERVER "xtrabackup --backup --stream=tar --datadir="$MY" --incremental-basedir="$BACKUPDIR"/"$CURRENTFULLBACKUP"/"$PREVIOUSBACKUP" | nc backup.navixy.com 9999" # make incr backup
-
-#    ssh root@"$SERVER "xtrabackup --backup --stream=tar --datadir="$MY" --incremental-lsn="$LAST_LSN"" | nc backup.navixy.com 9999" # make incr backup
-#    kill `pgrep "nc -l -p 9999" -f`
 
    cd "$TARGET" && ssh "$SERVER" xtrabackup --backup --incremental-lsn="$LAST_LSN" --datadir="$MY" --stream=xbstream | xbstream -x
    ssh "$SERVER" tar zcf - "$MY"/xtrabackup_logfile | tar zxf - --strip-components=3 
-#    rsync -rv --exclude=ibdata* --exclude=xtrabackup_* root@"$SERVER":"$MY" "$TARGET"
+
 ## backup the rest files    
    ssh "$SERVER" tar --create --preserve-permissions --totals --gzip  --ignore-failed-read --one-file-system --sparse\
     --wildcards --verbose \
@@ -106,8 +95,8 @@ rmexpiredbackups()
 {
 	
 	if [[ "$CURRENTNUMBEROFFULLBACKUPS" -ge "$NUMBEROFFULLBACKUPS" ]] && [[ "$NUMBEROFCURRENTINCRBACKUPS" -ge "$NUMBERINCRBACKUPS" ]] && [[ "$NUMBEROFINCREMENTALBACKUPSINOLDESTFULL" -ge "$NUMBERINCRBACKUPS" ]]
-        	then
-		echo -e "deleting old backups: "$BACKUPDIR"/"$OLDESTBACKUP"\n" 
+        then
+	    echo -e "deleting old backups: "$BACKUPDIR"/"$OLDESTBACKUP"\n" 
         rm -rf "$BACKUPDIR"/"$OLDESTBACKUP"
         ssh "$SERVER" 'rm -f "$MY"/backup.list'
 		echo -e "old backups deleted at `date +%F-%T`\n"  
@@ -117,7 +106,7 @@ rmexpiredbackups()
 	return 0;
 
 }
-#######################
+####### main() ##########
 
 
 
@@ -132,3 +121,4 @@ rmexpiredbackups()
  
 	rmexpiredbackups;
 
+## the end
